df58c8b65c83b0a39b5ccadcf4ad189760e0f6a4
Faisallm <faisallawan1997@gmail.com>
2023-09-04 23:46:27 -0700

    Done with keys

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let allKeys = [];
+
 // this creates a new audio element
 // by default audio src is a tune
 let audio = new Audio("tunes/a.wav");
@@ -8,18 +10,24 @@ const playTune = (key) => {
   audio.src = `tunes/${key}.wav`;
   audio.play();
 
+
   //    select the key that was clicked
   const clickedKey = document.querySelector(`[data-key="${key}"]`);
   clickedKey.classList.add("active");
+
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
+  }, 150);
 };
 
 // iterating through a list
 pianoKeys.forEach((key) => {
+  allKeys.push(key.dataset.key);
   key.addEventListener("click", () => playTune(key.dataset.key));
 });
 
 const pressedKey = (e) => {
-  playTune(e.key);
+  if (allKeys.includes(e.key)) playTune(e.key);
 };
 
 document.addEventListener("keydown", pressedKey);
